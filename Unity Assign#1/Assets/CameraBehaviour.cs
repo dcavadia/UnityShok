e499dfdf1adf6566065d3d7b9142955b1062d9f7
unknown <dcavadia@shokworks.io>
2019-12-09 16:33:30 -0400

    maze minigame

---
+++ Unity Assign#1/Assets/CameraBehaviour.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraBehaviour : MonoBehaviour
+{
+    public Rigidbody rigidB;
+    private Vector3 offset;
+    void Start()
+    {
+        offset = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = rigidB.transform.position + offset;
+    }
+}
+
---
+++ Unity Assign#1/Assets/NewBehaviourScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public Rigidbody rigidB;
+    public int speed = 10;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidB = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            rigidB.AddForce(Vector3.forward * speed);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            rigidB.AddForce(Vector3.back * speed);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            rigidB.AddForce(Vector3.right * speed);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            rigidB.AddForce(Vector3.left * speed);
+        }
+
+    }
+}
